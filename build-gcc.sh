163aa3cf9cc1c71c1f40131dad34b68a20753426
Daniel Jacob Chittoor <djchittoor47@gmail.com>
2021-05-03 00:03:04 +0530

    treewide: Align scripts with EditorConfig settings
    * shfmt -w .
    
    Signed-off-by: Daniel Jacob Chittoor <djchittoor47@gmail.com>

--- build-gcc.sh
+++ build-gcc.sh
@@ -8,16 +8,16 @@ echo "******************************"
 
 # TODO: Add more dynamic option handling
 while getopts a: flag; do
-	case "${flag}" in
-		a) arch=${OPTARG} ;;
-	esac
+  case "${flag}" in
+    a) arch=${OPTARG} ;;
+  esac
 done
 
 # TODO: Better target handling
 case "${arch}" in
-	"arm") TARGET="arm-eabi" ;;
-	"arm64") TARGET="aarch64-elf" ;;
-	"x86") TARGET="x86_64-elf" ;;
+  "arm") TARGET="arm-eabi" ;;
+  "arm64") TARGET="aarch64-elf" ;;
+  "x86") TARGET="x86_64-elf" ;;
 esac
 
 export WORK_DIR="$PWD"
@@ -27,61 +27,61 @@ export PATH="$PREFIX/bin:$PATH"
 echo "Building Bare Metal Toolchain for ${arch} with ${TARGET} as target"
 
 download_resources() {
-	echo "Downloading Pre-requisites"
-	git clone git://sourceware.org/git/binutils-gdb.git -b master binutils --depth=1
-	git clone https://git.linaro.org/toolchain/gcc.git -b master gcc --depth=1
-	cd ${WORK_DIR}
+  echo "Downloading Pre-requisites"
+  git clone git://sourceware.org/git/binutils-gdb.git -b master binutils --depth=1
+  git clone https://git.linaro.org/toolchain/gcc.git -b master gcc --depth=1
+  cd ${WORK_DIR}
 }
 
 build_binutils() {
-	cd ${WORK_DIR}
-	echo "Building Binutils"
-	mkdir build-binutils
-	cd build-binutils
-	../binutils/configure --target=$TARGET \
-		--prefix="$PREFIX" \
-		--with-sysroot \
-		--disable-nls \
-		--disable-docs \
-		--disable-werror \
-		--disable-gdb \
-		--enable-gold \
-		--with-pkgversion="Eva BinUtils"
-	make CFLAGS="-flto -O3 -pipe -ffunction-sections -fdata-sections" CXXFLAGS="-flto -O3 -pipe -ffunction-sections -fdata-sections" -j$(($(nproc --all) + 2))
-	make install -j$(($(nproc --all) + 2))
-	cd ../
+  cd ${WORK_DIR}
+  echo "Building Binutils"
+  mkdir build-binutils
+  cd build-binutils
+  ../binutils/configure --target=$TARGET \
+    --prefix="$PREFIX" \
+    --with-sysroot \
+    --disable-nls \
+    --disable-docs \
+    --disable-werror \
+    --disable-gdb \
+    --enable-gold \
+    --with-pkgversion="Eva BinUtils"
+  make CFLAGS="-flto -O3 -pipe -ffunction-sections -fdata-sections" CXXFLAGS="-flto -O3 -pipe -ffunction-sections -fdata-sections" -j$(($(nproc --all) + 2))
+  make install -j$(($(nproc --all) + 2))
+  cd ../
 }
 
 build_gcc() {
-	cd ${WORK_DIR}
-	echo "Building GCC"
-	cd gcc
-	./contrib/download_prerequisites
-	cd ../
-	mkdir build-gcc
-	cd build-gcc
-	../gcc/configure --target=$TARGET \
-		--prefix="$PREFIX" \
-		--disable-decimal-float \
-		--disable-libffi \
-		--disable-libgomp \
-		--disable-libmudflap \
-		--disable-libquadmath \
-		--disable-libstdcxx-pch \
-		--disable-nls \
-		--disable-shared \
-		--disable-docs \
-		--enable-default-ssp \
-		--enable-languages=c,c++ \
-		--with-pkgversion="Eva GCC" \
-		--with-newlib \
-		--with-gnu-as \
-		--with-gnu-ld \
-		--with-sysroot
-	make CFLAGS="-flto -O3 -pipe -ffunction-sections -fdata-sections" CXXFLAGS="-flto -O3 -pipe -ffunction-sections -fdata-sections" all-gcc -j$(($(nproc --all) + 2))
-	make CFLAGS="-flto -O3 -pipe -ffunction-sections -fdata-sections" CXXFLAGS="-flto -O3 -pipe -ffunction-sections -fdata-sections" all-target-libgcc -j$(($(nproc --all) + 2))
-	make install-gcc -j$(($(nproc --all) + 2))
-	make install-target-libgcc -j$(($(nproc --all) + 2))
+  cd ${WORK_DIR}
+  echo "Building GCC"
+  cd gcc
+  ./contrib/download_prerequisites
+  cd ../
+  mkdir build-gcc
+  cd build-gcc
+  ../gcc/configure --target=$TARGET \
+    --prefix="$PREFIX" \
+    --disable-decimal-float \
+    --disable-libffi \
+    --disable-libgomp \
+    --disable-libmudflap \
+    --disable-libquadmath \
+    --disable-libstdcxx-pch \
+    --disable-nls \
+    --disable-shared \
+    --disable-docs \
+    --enable-default-ssp \
+    --enable-languages=c,c++ \
+    --with-pkgversion="Eva GCC" \
+    --with-newlib \
+    --with-gnu-as \
+    --with-gnu-ld \
+    --with-sysroot
+  make CFLAGS="-flto -O3 -pipe -ffunction-sections -fdata-sections" CXXFLAGS="-flto -O3 -pipe -ffunction-sections -fdata-sections" all-gcc -j$(($(nproc --all) + 2))
+  make CFLAGS="-flto -O3 -pipe -ffunction-sections -fdata-sections" CXXFLAGS="-flto -O3 -pipe -ffunction-sections -fdata-sections" all-target-libgcc -j$(($(nproc --all) + 2))
+  make install-gcc -j$(($(nproc --all) + 2))
+  make install-target-libgcc -j$(($(nproc --all) + 2))
 
 }
 
