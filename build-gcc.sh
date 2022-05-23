dae64ca694e7d7f811296bfae109d003c7a27dae
Vaisakh Murali <mvaisakh@statixos.com>
2022-05-23 10:58:46 +0600

    Revert "build-gcc: Disable multilib support"
    This reverts commit 98648427e59849c72b3019c5c7fefa742adaa92e.
    This increased size of the compiler drastically.

--- build-gcc.sh
+++ build-gcc.sh
@@ -50,7 +50,6 @@ build_binutils() {
     CXXFLAGS="-flto -flto-compression-level=10 -O3 -pipe -ffunction-sections -fdata-sections" \
     --disable-docs \
     --disable-gdb \
-    --disable-mutlilib \
     --disable-nls \
     --disable-werror \
     --enable-gold \
@@ -83,7 +82,6 @@ build_gcc() {
     --disable-libmudflap \
     --disable-libquadmath \
     --disable-libstdcxx-pch \
-    --disable-mutlilib \
     --disable-nls \
     --disable-shared \
     --enable-default-ssp \
