3dec3b1bc6333f22178957e4883b61e1261086d1
Dakkshesh <dakkshesh5@gmail.com>
2023-02-19 10:14:52 +0530

    strip-bins: Use target verion of strip
    Host version of strip wont work with binaries targeting other architectures.
    
    Fixes:
    strip: Unable to recognise the format of the input file `/__w/gcc-build/gcc-arm64/lib/gcc/aarch64-elf/13.0.1/crtfastmath.o'
    strip: Unable to recognise the format of the input file `/__w/gcc-build/gcc-arm64/lib/gcc/aarch64-elf/13.0.1/crti.o'
    strip: Unable to recognise the format of the input file `/__w/gcc-build/gcc-arm64/lib/gcc/aarch64-elf/13.0.1/crtend.o'
    strip: Unable to recognise the format of the input file `/__w/gcc-build/gcc-arm64/lib/gcc/aarch64-elf/13.0.1/ilp32/crtfastmath.o'
    strip: Unable to recognise the format of the input file `/__w/gcc-build/gcc-arm64/lib/gcc/aarch64-elf/13.0.1/ilp32/crti.o'
    strip: Unable to recognise the format of the input file `/__w/gcc-build/gcc-arm64/lib/gcc/aarch64-elf/13.0.1/ilp32/crtend.o'
    strip: Unable to recognise the format of the input file `/__w/gcc-build/gcc-arm64/lib/gcc/aarch64-elf/13.0.1/ilp32/crtbegin.o'
    strip: Unable to recognise the format of the input file `/__w/gcc-build/gcc-arm64/lib/gcc/aarch64-elf/13.0.1/ilp32/crtn.o'
    strip: Unable to recognise the format of the input file `/__w/gcc-build/gcc-arm64/lib/gcc/aarch64-elf/13.0.1/crtbegin.o'
    strip: Unable to recognise the format of the input file `/__w/gcc-build/gcc-arm64/lib/gcc/aarch64-elf/13.0.1/crtn.o'
    
    Signed-off-by: Dakkshesh <dakkshesh5@gmail.com>

--- strip-binaries.sh
+++ strip-binaries.sh
@@ -1,8 +1,25 @@
 #!/bin/bash
 
 CUR_DIR=$(pwd)
+X86S=$(which strip)
+A64S=$(which aarch64-linux-gnu-strip)
+A32S=$(which arm-linux-gnu-strip)
 
-for f in $(find $CUR_DIR -type f -exec file {} \; | grep 'not stripped' | awk '{print $1}'); do
-  f="${f::-1}"
-  strip "${f}"
-done
+find "$CUR_DIR" -type f -exec file {} \; >.file-idx
+
+grep "x86" .file-idx |
+	grep "not strip" | grep -v "relocatable" |
+	tr ':' ' ' | awk '{print $1}' |
+	while read -r file; do $X86S "$file"; done
+
+grep "ARM" .file-idx | grep "aarch64" |
+	grep "not strip" | grep -v "relocatable" |
+	tr ':' ' ' | awk '{print $1}' |
+	while read -r file; do $A64S "$file"; done
+
+grep "ARM" .file-idx | grep "32.bit" |
+	grep "not strip" | grep -v "relocatable" |
+	tr ':' ' ' | awk '{print $1}' |
+	while read -r file; do $A32S "$file"; done
+
+rm ".file-idx"
